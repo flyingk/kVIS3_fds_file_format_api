c4f2adbb3c8ac8752ee62aabd9e95b841a76af67
Kai Lehmkuehler <kai@uav-flightresearch.com>
2021-03-09 17:07:34 +0100

    get group/channel working

--- query/kVIS_fdsGetChannel.m
+++ query/kVIS_fdsGetChannel.m
@@ -1,87 +1,81 @@
+%
+%> @file kVIS_fdsGetChannel.m
+%> @brief Returns data content of a channel
+%
+%
 % kVIS3 Data Visualisation
 %
 % Copyright (C) 2012 - present  Kai Lehmkuehler, Matt Anderson and
 % contributors
 %
 % Contact: kvis3@uav-flightresearch.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [signal, signalMeta] = kVIS_fdsGetChannel(fds, group, channel)
-
-% returns the requested channel from the fds structure
 %
-%  [data, fieldno, channelno] = fds_get_channel(fds, field, channel)
+%> @brief Returns data content of a channel
+%>
+%> @param fds data structure
+%> @param Group name to search
+%> @param Channel name to search
+%> @param Optional additional UNIQUE group identifier (if group name not unique)
+%>
+%> @retval Data content of group as array or -1 if not found
+%> @retval Meta data for channel or -1 if not found
 %
-% Matt and Kai 2017
+function [signal, signalMeta] = kVIS_fdsGetChannel(fds, groupName, channel, varargin)
 
-% find the array number
-group_found = 0;
-groupNo = 1;
+% group name might exist -> channel could be duplicated as well -> need to
+% find the correct group
 
-while (~group_found)
-    % Get field name
-    current_field = fds.fdata{fds.fdataRows.groupLabel, groupNo};
-    
-    % Check for match (case insensitive)
-    if (strcmpi(current_field, group))
-        % Channel has been found
-        group_found = 1;
-         
-    else
-        % Increment counter and try again
-        groupNo = groupNo + 1;
-        
-        % Check for end of array
-        if groupNo > size(fds.fdata,2)
-            signal = -1;
-            signalMeta = -1;
-            fprintf('Can''t find group << %s >>\n', group);
-            return
-        end
-        
-    end
-    
+% channel is unique in a group, so if group is unique, then channel will be
+% as well
+
+if ~isstruct(fds)
+    disp('No fds structure specified...')
+    signal = -1;
+    signalMeta = -1;
+    return
 end
 
-% find the channel number
-channel_found = 0;
-channel_idx=1;
 
-while (~channel_found)
-    % Get channel name
-    current_channel = fds.fdata{fds.fdataRows.varNames, groupNo}{channel_idx};
-    
-    % Check for match (case sensitive)
-    if (strcmp(strip(current_channel),channel))
-        % Channel has been found
-        channel_found = 1;
-        
-    else
-        % Increment counter and try again
-        channel_idx = channel_idx + 1;
-        
-        % Check for end of array
-        if channel_idx > length(fds.fdata{fds.fdataRows.varNames,groupNo})
-            signal = -1;
-            signalMeta = -1;
-            fprintf('Can''t find channel << %s >> in group << %s >>\n', channel, group);
-            return
-        end
-        
-    end
+if nargin > 3
+    id2 = varargin{1};
+    [~, groupNo] = kVIS_fdsGetGroup(fds, groupName, id2);
+else
+    [~, groupNo] = kVIS_fdsGetGroup(fds, groupName);
 end
+
+
+if groupNo == -1
+    disp('kVIS_fdsGetChannel: Invalid group name.')
+    signal = -1;
+    signalMeta = -1;
+    return
+end
+
+
+% find channel name in group
+channel_idx = strcmp(fds.fdata{fds.fdataRows.varNames, groupNo}, channel);
+
+if ~any(channel_idx)
+    disp('kVIS_fdsGetChannel: Invalid channel name.')
+    signal = -1;
+    signalMeta = -1;
+    return
+end
+
 %
 % signal data
 %
@@ -95,5 +89,6 @@ signalMeta.frame     = fds.fdata{fds.fdataRows.varFrames, groupNo}{channel_idx};
 signalMeta.dispName  = fds.fdata{fds.fdataRows.varNamesDisp, groupNo}{channel_idx};
 signalMeta.dataSet   = [];
 signalMeta.dataGroup = fds.fdata{fds.fdataRows.groupLabel, groupNo};
+signalMeta.dataPath  = kVIS_fdsBuildTreePath(fds, fds.fdata{fds.fdataRows.groupID, groupNo});
 signalMeta.timeVec   = fds.fdata{fds.fdataRows.data, groupNo}(:, 1);
 end
--- query/kVIS_fdsGetGroup.m
+++ query/kVIS_fdsGetGroup.m
@@ -1,52 +1,104 @@
+%
+%> @file kVIS_fdsGetGroup.m
+%> @brief Return data content of a tree group
+%
+%
 % kVIS3 Data Visualisation
 %
 % Copyright (C) 2012 - present  Kai Lehmkuehler, Matt Anderson and
 % contributors
 %
 % Contact: kvis3@uav-flightresearch.com
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function [data, index] = kVIS_fdsGetGroup(fds, field_name)
-% Finds a match for the field name and gives you the output
+%
+%> @brief Return data content of a tree group
+%>
+%> @param fds data structure
+%> @param Group name to search
+%> @param Optional additional UNIQUE group identifier (if group name not unique)
+%>
+%> @retval Data content of group as array or -1 if not found
+%> @retval fds.fdata column index of the group
+%
+function [data, index] = kVIS_fdsGetGroup(fds, groupName, varargin)
 
-if ~nargin
+if ~isstruct(fds)
     disp('No fds structure specified...')
     data = -1;
     index = -1;
     return
 end
 
-% Find the correct index by trying to match the name
-i=1;
+idx = kVIS_fdsGetGroupLabelColumnIndex(fds, groupName);
 
-while (strcmp(fds.fdata{fds.fdataRows.groupLabel,i}, field_name) == false)
-    i=i+1;
+
+if (idx == -1)
+    fprintf('Group name %s does not exist\n',groupName);
+    data = -1;
+    index = -1;
+    return
     
-    % Check if we've reached the end of the data set
-    if (i > size(fds.fdata,2))
-        fprintf('Data file name %s does not exist\n',field_name);
+elseif length(idx) > 1
+    % multiple entries with identical name
+    
+    if nargin > 2
+        
+        % Use additional identifier
+        ID2 = varargin{1};
+        
+        % Search corresponding idx in full tree path
+        for I = 1:length(idx)
+            
+            groupID = fds.fdata{fds.fdataRows.groupID, idx(I)};
+            
+            treePath = kVIS_fdsBuildTreePath(fds, groupID);
+            
+            % use / so parts of strings won't be found??
+            found(I) = contains(treePath, ID2);
+            
+        end
+        
+        idx2 = find(found==true);
+        
+        if length(idx2) == 1
+            
+            data = fds.fdata{fds.fdataRows.data, idx(idx2)};
+            index = idx(idx2);
+            
+        else
+            
+            errordlg('kVIS_fdsGetGroup: Multiple/no entries found...Abort')
+            data = -1;
+            index = -1;
+            
+        end
+        
+    else
+        
+        errordlg('kVIS_fdsGetGroup: Multiple entries found...Abort')
         data = -1;
         index = -1;
-        return
     end
     
+    
+    return
+else
+    data = fds.fdata{fds.fdataRows.data,idx};
+    index = idx;
 end
-
-% Index found, get the data
-data = fds.fdata{fds.fdataRows.data,i};
-index = i;
 
 return
 end
